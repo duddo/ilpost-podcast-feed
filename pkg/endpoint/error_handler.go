9c6228741daa1d6204467cffa7a5100c06f80c0e
duddo <dvd100@gmail.com>
2024-10-20 16:43:44 +0200

    Added user-agent to logging

--- pkg/endpoint/error_handler.go
+++ pkg/endpoint/error_handler.go
@@ -20,15 +20,37 @@ ERROR %d
 	%s%s`
 
 func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Serving %s %s", r.Method, r.RequestURI)
+	// Extract the username from basic authentication
+	username := ""
+	username, _, ok := r.BasicAuth()
+	if ok {
+		username = " | User: " + username
+	}
+
+	log.Printf(
+		"%s %s%s | %s | User-Agent: %s",
+		r.Method,      // HTTP method (GET, POST, etc.)
+		r.RequestURI,  // Full request URI
+		username,      // Client's user agent string
+		r.Proto,       // Protocol (HTTP/1.1, etc.)
+		r.UserAgent(), // Username from basic authentication or "anonymous"
+	)
+
 	ae := fn(w, r)
+
 	if ae != nil {
 		errorString := ""
 		if ae.Error != nil {
 			errorString = ": " + ae.Error.Error()
 		}
 
-		log.Printf("ERROR %d - Request %s %s - %s%s", ae.Code, r.Method, r.RequestURI, ae.Message, errorString)
+		log.Printf("ERROR %d | %s %s | %s%s | User-Agent: %s",
+			ae.Code,
+			r.Method,
+			r.RequestURI,
+			ae.Message,
+			errorString,
+			r.UserAgent())
 
 		message := fmt.Sprintf(httpErrorResponsePage, ae.Code, ae.Message, errorString)
 		http.Error(w, message, ae.Code)
